5e790f75014e8fd1fa1830ac2e3879b5749bdb04
mattygersh21 <dakotaseia@hotmail.com>
2025-01-04 23:39:43 -0600

    * added rest template configuration * added "/gateway" to all controller endpoints * added "/login" endpoint which forwards requests to pam microservice * added LoginRequest class * added LoginResponse class * included pam.service.url in application properties * updated testHome test and test class as needed

--- src/main/java/emsquare/roomie_find/gateway/controllers/ValidateCallController.java
+++ src/main/java/emsquare/roomie_find/gateway/controllers/ValidateCallController.java
@@ -1,14 +1,55 @@
 package emsquare.roomie_find.gateway.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import emsquare.roomie_find.gateway.dtos.LoginRequest;
+import emsquare.roomie_find.gateway.dtos.LoginResponse;
 
 @RestController
+@RequestMapping("/gateway")
 public class ValidateCallController {
+
+    @Value("${pam.service.url}")
+    private String pamServiceUrl;
+        
+    private final RestTemplate restTemplate;
+    
+    public ValidateCallController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     @GetMapping("/")
     public String home() {
         return "Application is running!";
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+        String pamUrl = UriComponentsBuilder.fromUriString(pamServiceUrl)
+                .path("/login")
+                .toUriString();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json");
+
+        HttpEntity<LoginRequest> pamEntity = new HttpEntity<>(loginRequest, headers);
+
+        ResponseEntity<LoginResponse> pamResponse = restTemplate.exchange(pamUrl, HttpMethod.POST, pamEntity, LoginResponse.class);
+
+        return ResponseEntity.status(pamResponse.getStatusCode())
+                             .body(pamResponse.getBody());
+    }
+
     
 }
--- src/test/java/emsquare/roomie_find/gateway/controllers/ValidateCallControllerTest.java
+++ src/test/java/emsquare/roomie_find/gateway/controllers/ValidateCallControllerTest.java
@@ -6,7 +6,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.RestTemplate;
 
 
 
@@ -16,9 +18,12 @@ public class ValidateCallControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockitoBean
+    private RestTemplate restTemplate;
+
     @Test
     public void testHome() throws Exception {
-        mockMvc.perform(get("/"))
+        mockMvc.perform(get("/gateway/"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Application is running!"));
     }
